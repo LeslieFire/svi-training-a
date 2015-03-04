09e1c7390ff22d79b314c5ca35907375eef8ba62
Antonio Carlos Ortiz <ortizantoniocarlos@gmail.com>
2015-03-04 23:17:08 +0800

    refactored oglafparsercarlos_0.py

--- code-samples/week1/scraping training exercises/oglaf/oglafparsercarlos_0.py
+++ code-samples/week1/scraping training exercises/oglaf/oglafparsercarlos_0.py
@@ -1,7 +1,8 @@
 '''
 Written by Antonio Carlos L. Ortiz 02/25/2015
 Input: website - 'http://oglaf.com/archive/'
-Output: scraped images of the comics with the title and alt description appendend at the top of the photo.
+Output: scraped images of the comics with the title and alt description appendend at the
+top of the photo.
 '''
 
 import requests
@@ -14,176 +15,128 @@ import sys
 from image_utils import ImageText
 
 
-#takes in the image source, and local filename then downloads the image to out/localname
 def download(src, localname):
-	image = get_page(src, 5)
-	if not image:
-		return False
+    '''
+    takes in the image source, and local filename then downloads the image to out/localname.
+    '''
+    image = get_page(src, 5)
+    if not image:
+        return False
 
-	f = open('out/' + localname, 'wb')
-	f.write(image)
-	f.close()
+    f = open('out/' + localname, 'wb')
+    f.write(image)
+    f.close()
 
-	return True
+    return True
 
-def make_page(infile, outfile, text = "", text2 = ""): # takes the inputfilename, outputfilename and text to put it in at the bottom
+def make_page(infile, outfile, text = "", text2 = ""):
+    '''takes the inputfilename, outputfilename and text to put it in at the bottom'''
 
-	font = 'Aller_Rg.ttf' #some random font, just fine one that is legible enough
-	color = (50, 50, 50) # color of the text box
-	page = Image.open(infile) # opening original image
-	width, original_height = page.size # size of original image
-	temp = ImageText((1000, 500), background = (255, 255, 255, 200)) # making a large temp Image text object to put  the text in.
-	height = temp.write_text_box((0, 0), text, box_width = width, font_filename = font, font_size = 16, color = color)[1] + 20 # +20 y offset writer leaves that much space at the top
-	textbox = temp.image.crop((0, 0, width, height)) #crop textbox
-	
-	temp = ImageText((1000, 500), background = (255, 255, 255, 200)) # making a large temp Image text object to put  the text in.
-	height2 = temp.write_text_box((0, 0), text2, box_width = width, font_filename = font, font_size = 16, color = color)[1] + 20# +20 y offset writer leaves that much space at the top
-	textbox2 = temp.image.crop((0, 0, width, height)) #crop textbox
-	
-	output = Image.new("RGBA", (width, original_height + height + height2), (120, 20, 20)) # make new blank image with computed dimensions
-	output.paste(textbox2, (0,0))
-	output.paste(page, (0,height2)) # paste original
-	output.paste(textbox, (0, original_height + height2)) # paste textbox
-	output.save(outfile) # save file
+    font = 'Aller_Rg.ttf' #some random font, just fine one that is legible enough
+    color = (50, 50, 50) # color of the text box
+    page = Image.open(infile) # opening original image
+    width, original_height = page.size # size of original image
 
+    temp = ImageText((1000, 500), background=(255, 255, 255, 200))
+    
+    # +20 y offset writer leaves that much space at the top
+    height = temp.write_text_box((0, 0), text,
+                                 box_width=width, font_filename=font,
+                                 font_size=16, color=color)[1] + 20
+    
+    textbox = temp.image.crop((0, 0, width, height)) #crop text
+    #making a large temp Image text object to put  the text in.
+    temp = ImageText((1000, 500), background=(255, 255, 255, 200)) 
+    
+    # +20 y offset writer leaves that much space at the top
+    height2 = temp.write_text_box((0, 0), text2,
+                                  box_width=width, font_filename=font,
+                                  font_size=16, color=color)[1] + 20
+    
+    textbox2 = temp.image.crop((0, 0, width, height)) #crop textbox
+    
+    output = Image.new("RGBA", (width, original_height + height + height2),
+                      (120, 20, 20)) # make new blank image with computed dimensions
+    output.paste(textbox2, (0,0))
+    output.paste(page, (0,height2)) # paste original
+    output.paste(textbox, (0, original_height + height2)) # paste textbox
+    output.save(outfile) # save file
 
-#function to check if the requested page returns the correct page and did not time out.
 def get_page(url, max_attempts):
-	for i in xrange(max_attempts):
-		print "attempt no.", i + 1
-		r = requests.get(url)
-		
-		if r.status_code == 200:
-			return r.content
+    '''
+    Function to check if the requested page returns the correct page and did not
+    time out.
+    '''
+    for i in xrange(max_attempts):
+        print "attempt no.", i + 1
+        r = requests.get(url)
+        
+        if r.status_code == 200:
+            return r.content
 
-	print r.status_code
-	return False
+    print r.status_code
+    return False
 
-def extract_archive(link):
-	'''
-	link should be the archive page of oglaf.
-	1st. It gets all the clickable links in the archive page.
-	2nd. The function goes inside the link then saves the comic there to local,
-	3rd. The function then checks if the comic has multiple parts that can only be downloaded on the pages after the current.
-
-		If there are, it goes through all those pages, saves them to local, until it returns False.
-		If there isnt any other pages, the function goes back to "for i in comic:" to try to download the next comic.
-
-
-	'''
-	#tries to open the page with a 5 attempt limit. 
-	html = get_page(link, 5)
+def extract_archive(url1, url2):
+    '''extract links on the archive page'''
+    html = get_page(url1, 5)
  
-	# if html is not false. This will only become false if get_page() from above did not return anything.
-	if html:
-		soup = BeautifulSoup(html)
+    if html:
+        soup = BeautifulSoup(html)
+        comic = soup.findAll('a')   #find all tags <a href= ..
+        
+        for i in comic:
+            #method 'get()'' can obtain data with a var=data format
+            address = i.get('href')        
+            new_addr = "".join((url2,address[1:]))
+            
+            html_inner = get_page(new_addr, 5)
+            
+            extract_comic(html_inner, address, new_addr)
 
-		#find all tags <a href= ..
-		comic = soup.findAll('a')
-		for i in comic:
-			# method 'get()'' can obtain data with a var=data format			
-			# code below gets value of href in i.
-			address = i.get('href')
+def extract_comic(html_inner,address, new_addr, count=1):
+    '''
+    find the element with an id of 'strip' as this contains details of the comic in html_inner
+    '''
+    if html_inner:
+        soup_inner = BeautifulSoup(html_inner)
+        comic = soup_inner.find(id = 'strip')
 
+        src = comic.get('src')
+        alt = comic.get('alt')
+        title = comic.get('title')
 
-			#to create a new address using 'address' that we got from get
-			joiner = ""			
-			new_addr = joiner.join(('http://oglaf.com/',address[1:])) #avoid hardcoding
-			print new_addr
+        #create the filename for the comic
+        filename = address[1:][:-1] + '-' + string.zfill(1, 3) + '.' + \ 
+            src[-3:].lower()
 
-			#requests with 5 attempts until get_page() returns false
-			html_inner =  get_page(new_addr, 5)
+        # save the picture to local.
+        download(src, filename)
+        
+        try:
+            make_page(os.path.join('out', filename), os.path.join(outpath,
+                os.path.basename(filename)), title, alt) # process the page
+        except Exception, e:
+            print e
+        
+        #printing is buffered by default and this makes the display  as soon as it is encountered
+        sys.stdout.flush()
 
-			# if html is not false. This will only become false if get_page() from above did not return anything.
-			if html_inner:
-				
-				'''
-				find the element with an id of 'strip' as this contains details of the comic in html_inner
-				'''
-				soup_inner = BeautifulSoup(html_inner)
-				comic = soup_inner.find(id = 'strip')
-				print comic
-
-				src = comic.get('src')
-				alt = comic.get('alt')
-				title = comic.get('title')
-
-
-				#create the filename for the comic
-				filename = address[1:][:-1] + '-' + string.zfill(1, 3) + '.' + src[-3:].lower()
-				print filename
-
-				# save the picture to local.
-				download(src, filename)
-
-				#okay so it works but i still have to study it so I can put the alt at the top
-				try:
-					make_page(os.path.join('out', filename), os.path.join(outpath, os.path.basename(filename)), title, alt) # process the page
-				except Exception, e:
-					print e
-				sys.stdout.flush() # printing is buffered by default and this makes the display  as soon as it is encountered
-
-				comic = soup_inner.find(href = address + '2/')
-
-				#this is for the comic segments with multiple pages.
-				if comic:
-					# this should be a new function
-					count = 2
-					while True:
-						
-						new_addr_2  = new_addr + str(count) + '/'
-						new_page = get_page(new_addr_2,2)
-
-						if new_page: 
-							#if the commic has another page, repeat the process above.
-							# any process that repeats ought to be turned into a function 
-							
-							# pseudocode: 
-							# download_page(url)
-							# while next_page_exists():
-							#	download_page(next_page_url)
-
-							new_page = BeautifulSoup(new_page)
-							comic2 = new_page.find(id = 'strip')
-
-							src = comic2.get('src')
-							alt = comic2.get('alt')
-							title = comic2.get('title')
-
-							filename = address[1:][:-1] + '-' + string.zfill(count, 3) + '.' + src[-3:].lower()
-							download(src, filename)
-
-
-							print filename
-							print os.path.join(outpath, os.path.basename(filename))
-							print title
-							print alt
-
-							try:
-								make_page(os.path.join('out', filename), os.path.join(outpath, os.path.basename(filename)), title, alt)
-							except Exception, e:
-								print e
-
-							sys.stdout.flush()
-
-							count = count + 1
-
-							comic2 = new_page.find(href = address + str(count) + '/')
-
-							if comic2:
-								continue
-							else:
-								break
-
-
+        
+        count += 1
+        html_inner = soup_inner.find(href = address + str(count) + '/')
+        
+        #this is for the comic segments with multiple pages.
+        extract_comic(html_inner,address,new_addr,count)
 
 if __name__ == "__main__":
+    '''create directory if it is not yet existing'''
+    for directory in ['out', 'final']:
+        if not os.path.exists(directory):
+            os.makedirs(directory)
 
-	for directory in ['out', 'final']:
-		if not os.path.exists(directory):
-			os.makedirs(directory)
+    outpath = os.path.join(os.getcwd(), 'final')
 
-	outpath = os.path.join(os.getcwd(), 'final')
-
-	url = 'http://oglaf.com/archive/'
-	extract_archive(url)
+    url1 = 'http://oglaf.com/archive/'
+    url2 = 'http://oglaf.com/'
+    extract_archive(url1, url2)
